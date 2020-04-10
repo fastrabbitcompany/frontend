66ff85f1fe257c074071fe3502cc0b887177d0b0
Juan Camilo Gomez Lopez <juankg214@gmail.com>
2020-04-10 14:23:22 -0500

    Added Home initial files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import {Route, Switch} from "react-router-dom";
 import Login from "./pages/login/Login.component"
 import Register from "./pages/register/Register.component"
+import Home from "./pages/homepage/Home.component"
 
 const App = () => (
   <Switch>
     <Route exact path="/" component = {Login}/>
     <Route exact path="/register" component = {Register}/>
+    <Route exact path="/home" component = {Home}/>
   </Switch>
 );
   
