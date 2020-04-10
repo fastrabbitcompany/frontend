9b8893414b4d39b70fe0967e2d5a7d194a18561f
Juan Camilo Gomez Lopez <juankg214@gmail.com>
2020-04-10 01:37:20 -0500

    register

--- src/pages/register/Register.component.jsx
+++ src/pages/register/Register.component.jsx
@@ -1,24 +1,73 @@
 import React from "react";
-import { Form, Col, Button, Container, Row } from "react-bootstrap";
+import { Form, Col, Button, Container, Row, Card } from "react-bootstrap";
 import "./Register.styles.css";
 
-const Register = (props) => {
-  return (
-    <div className="register h-100 d-flex justify-content-center align-items-center">
-      <Form className="mx-4 ">
-     
-  <Form.Group controlId="formGroupEmail">
-    
-    <Form.Control type="email" placeholder="Enter email" />
-  </Form.Group>
-  <Form.Group controlId="formGroupPassword">
- 
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
+class Register extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+  return(
+    <div className="register justify-content-center align-items-center">
+      <Card className="card-register m-4 shadow-lg">
+        <Card.Body>
+          <Card.Title className="title-register">Create Account</Card.Title>
+          <Form className="my-4">
+            <Row md={2} sm={1} xs={1}>
+              <Col>
+                <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+                  <Form.Label className="label-register">First Name</Form.Label>
+                  <Form.Control type="text" placeholder="Jhon" required/>
+                </Form.Group>
+              </Col>
+              <Col >
+                <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+                  <Form.Label className="label-register">Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="Doe" required/>
+                </Form.Group>
+              </Col>
+            </Row>
+            
+            <Row md={2} sm={1} xs={1}>
+              <Col  md={8} lg={8}>
+                <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+                  <Form.Label className="label-register">Address</Form.Label>
+                  <Form.Control type="text" placeholder="Cll 26a No. 24a-18" required />
+                </Form.Group>
+              </Col>
+              <Col md={4} lg={4}>
+                <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+                  <Form.Label className="label-register">Phone number</Form.Label>
+                  <Form.Control type="number" placeholder="3104787326" required />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+              <Form.Label className="label-register">E-mail</Form.Label>
+              <Form.Control type="email" placeholder="example@gmail.com" required/>
+            </Form.Group>
 
-      </Form>
+            
+
+            <Form.Group style={{ textAlign: "left" }} controlId="formBasicEmail">
+              <Form.Label className="label-register">Password</Form.Label>
+              <Form.Control type="password" placeholder="password" required/>
+            </Form.Group>
+
+            <Button variant="none" className="reg w-100" type="submit"> Register </Button>
+            <div className="d-flex  justify-content-center align-items-center">
+            <small>Already have an account?</small>
+            <Button variant="link" size="sm"
+            onClick = {() => this.props.history.push("/")}>Sign In</Button>
+            </div>
+           
+
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   );
+  }
 };
 
 export default Register;
