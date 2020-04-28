df03623f0056688562d746bbae1c3258cc7297de
stephan <dsramirezc@unal.edu.co>
2020-04-28 04:38:50 -0500

    crear envio

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Route, Switch} from "react-router-dom";
 import Login from "./pages/login/Login.component"
 import Register from "./pages/register/Register.component"
 import Home from "./pages/homepage/home"
+import createShipping from "./pages/createShipping/createShipping"
 
 const App = () => (
   <Switch>
     <Route exact path="/" component = {Login}/>
     <Route exact path="/register" component = {Register}/>
     <Route exact path="/home" component = {Home}/>
+    <Route exact path="/createShipping" component = {createShipping}/>
   </Switch>
 );
   
